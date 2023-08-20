96a6bc6d06a11f29e3ed18773d324640ef985f91
yamato0211 <kumayama0211@gmail.com>
2023-08-21 08:29:39 +0900

    change: user & chara repository

--- pkg/domain/repository/character.go
+++ pkg/domain/repository/character.go
@@ -7,4 +7,5 @@ import (
 
 type CharacterRepository interface {
 	SelectAllByID(ctx context.Context, userID int64) ([]entity.Character, error)
+	SelectByUserID(ctx context.Context, userID int64, characterID int64) (entity.Character, error)
 }
--- pkg/domain/repository/user.go
+++ pkg/domain/repository/user.go
@@ -6,7 +6,7 @@ import (
 
 type UserRepository interface {
 	Insert(ctx context.Context, name string, token string) (string, error)
-	SelectNameByToken(ctx context.Context, token string) (string, error)
+	SelectNameByUserID(ctx context.Context, userID int64) (string, error)
 	SelectIDByToken(ctx context.Context, token string) (int64, error)
-	Update(ctx context.Context, name string, token string) error
+	Update(ctx context.Context, name string, userID int64) error
 }
