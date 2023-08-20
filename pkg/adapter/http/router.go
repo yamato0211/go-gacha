afb53431518baca7ad43a81b6aefba8dd2192142
yamato0211 <kumayama0211@gmail.com>
2023-08-21 08:30:54 +0900

    change: router middleware

--- pkg/adapter/http/router.go
+++ pkg/adapter/http/router.go
@@ -1,6 +1,7 @@
 package http
 
 import (
+	"go-gacha-system/pkg/adapter/http/middleware"
 	"go-gacha-system/pkg/infra"
 	"go-gacha-system/pkg/infra/mysql"
 	"go-gacha-system/pkg/usecase"
@@ -16,16 +17,31 @@ import (
 func InitRouter() *gin.Engine {
 	r := gin.Default()
 	r.Use(cors.Default())
-
+	//DI
 	mySQLConn := infra.NewMySQLConnector()
 	userRepository := mysql.NewUserRepository(mySQLConn.DB)
+	characterRepository := mysql.NewCharacterRepository(mySQLConn.DB)
+	gachaRepository := mysql.NewGachaRepository(mySQLConn.DB)
 	userUsecase := usecase.NewUserUsecase(userRepository)
+	characterUsecase := usecase.NewCharacterUsecase(characterRepository, userRepository)
+	gachaUsecase := usecase.NewGachaUsecase(gachaRepository, characterRepository, userRepository)
+	middleware := middleware.NewAuthMiddleware(userRepository)
 	user := r.Group("/user")
 	{
 		handler := NewUserHandler(userUsecase)
-		user.GET("/get", handler.GetUser())
+		user.GET("/get", middleware.Authenticate(), handler.GetUser())
 		user.POST("/create", handler.CreateUser())
-		user.PUT("/update", handler.UpdateName())
+		user.PUT("/update", middleware.Authenticate(), handler.UpdateName())
+	}
+	character := r.Group("/character")
+	{
+		handler := NewCharacterHandler(characterUsecase)
+		character.GET("/list", middleware.Authenticate(), handler.GetCharacters())
+	}
+	gacha := r.Group("/gacha")
+	{
+		handler := NewGachaHandler(gachaUsecase)
+		gacha.POST("/draw", middleware.Authenticate(), handler.DrawGacha())
 	}
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
