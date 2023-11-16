e2fe6e8ae24cbc248747f18ef8fe65d2d4b11516
yamato0211 <kumayama0211@gmail.com>
2023-11-16 17:04:00 +0900

    add: http adapter

--- pkg/adapter/http/character_handler.go
+++ pkg/adapter/http/character_handler.go
@@ -21,16 +21,16 @@ func NewCharacterHandler(usecase usecase.ICharacterUsecase) *characterHandler {
 
 //	GetCharacters godoc
 //
-// @Summary		キャラクター関連API
-// @Description	ユーザが所持しているキャラクター一覧情報を取得
-// @Tags			character
-// @Accept			json
-// @Produce		json
-// @Param			x-token	header		string	true	"認証トークン"
-// @Success		200		{object}	[]entity.Character
-// @Failure		400		{object}	error
-// @Failure		500		{object}	error
-// @Router			/character/list [get]
+//	@Summary		キャラクター関連API
+//	@Description	ユーザが所持しているキャラクター一覧情報を取得
+//	@Tags			character
+//	@Accept			json
+//	@Produce		json
+//	@Param			x-token	header		string	true	"認証トークン"
+//	@Success		200		{object}	[]entity.Character
+//	@Failure		400		{object}	error
+//	@Failure		500		{object}	error
+//	@Router			/character/list [get]
 func (ch *characterHandler) GetCharacters() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		userID := utils.GetUserIDFromContext(c)
